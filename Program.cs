af6be35086fa995c69f6e76dce067e085e4736d7
Bre4dGC <glebbreadc@gmail.com>
2024-03-18 19:36:43 +0300

    secomd commit

--- Program.cs
+++ Program.cs
@@ -1,17 +1,22 @@
 ﻿Enemy boss = new Enemy();
 User player = new User();
-Random randBossInput = new Random();
-Console.Write($"Придумайте себе ник: ");
-player.UserName = Console.ReadLine(); Console.Clear();
 
-Console.Write("Легенда\nВ краю, где царствует тьма, прозябал зловещий Теневой Босс – непобедимый и коварный. " +
+Random randBossInput = new Random();
+
+Console.Write($"Придумайте себе ник: ");
+player.Name = Console.ReadLine();
+
+Console.Write
+    ("\nЛегенда\nВ краю, где царствует тьма, прозябал зловещий Теневой Босс – непобедимый и коварный. " +
     "\nНо в центре этой темницы среди тьмы стоял Маг, некогда великий, но когда-то побежденный. " +
     "\nЕго душа дремала, ожидая момента, когда он снова станет тем, кем был прежде. " +
     "\nПоднявшись из пепла, Маг ощутил множество эмоций – гнев, решимость, пробуждение внутренней магии. " +
     "\nЕго арсенал был полон заклинаний, способных пробудить силы, замирающие в мире тьмы. " +
-    "\nС увиденным врагом лицом к лицу, он знал, что долг его – покончить с этой угрозой, иначе ему не предстоит покоя."); Console.ReadKey(); Console.Clear();
+    "\nС увиденным врагом лицом к лицу, он знал, что долг его – покончить с этой угрозой, иначе ему не предстоит покоя."); 
 
-while(player.PlayerHealth > 0 && boss.BossHealth > 0)
+Console.ReadKey(); Console.Clear();
+
+while(player.Health > 0 && boss.BossHealth > 0)
 {
     CharactersHealth(boss, player);
     Console.Write("Введите номер атаки: ");
@@ -20,111 +25,141 @@ while(player.PlayerHealth > 0 && boss.BossHealth > 0)
     {
         case "1":
             CharactersHealth(boss, player);
-            player.Huganzakura(boss); Clean();
+            player.Huganzakura(boss); 
+            Clean();
             break;
+
         case "2":
             CharactersHealth(boss, player);
-            player.magicalAttack(boss); Clean();
+            player.MagicalAttack(boss); 
+            Clean();
             break;
+
         case "3":
             CharactersHealth(boss, player);
-            player.Healing(boss); Clean();
+            player.Healing(); 
+            Clean();
             break;
     }
 
     int bossAttack = randBossInput.Next(1, 3);
+
     switch (bossAttack)
     {
         case 1:
             CharactersHealth(boss, player);
-            boss.Rashamon(player); Clean();
+            boss.Rashamon(player); 
+            Clean();
             break;
+
         case 2:
             CharactersHealth(boss, player);
-            boss.ShadowCloning(player); Clean();
+            boss.ShadowCloning(player); 
+            Clean();
             break;
     }
-    if (boss.BossHealth <= 200 && boss.PotionCount > 0)
+    if (boss.BossHealth <= 150 && boss.PotionCount > 0 && player.Health > 0)
     {
-        CharactersHealth(boss, player); boss.Healing(); Clean();
+        CharactersHealth(boss, player); 
+        boss.Healing(); 
+        Clean();
     }
 }
+
 if (boss.BossHealth <= 0)
 {
     Console.Clear();
-    Console.WriteLine($"Игрок {player.UserName} победил босса");
+    Console.WriteLine($"Игрок {player.Name} победил босса");
 }
-else if (player.PlayerHealth <= 0)
+else if (player.Health <= 0)
 {
     Console.Clear();
-    Console.WriteLine($"Босс победил игрока {player.UserName}");
+    Console.WriteLine($"Босс победил игрока {player.Name}");
 }
+
 static void Clean()
 {
-    Console.ReadKey(); Console.Clear();
+    Console.ReadKey(); 
+    Console.Clear();
 }
 
 static void CharactersHealth(Enemy boss, User player)
 {
     Console.SetCursorPosition(0, 5);
-    Console.WriteLine($"| Хп босса - {boss.BossHealth}, броня босса - {boss.BossArmor} | Хп {player.UserName} - {player.PlayerHealth}, броня {player.UserName} - {player.PlayerArmor}");
+    Console.WriteLine($"| Хп босса - {boss.BossHealth}, броня босса - {boss.BossArmor} | Хп {player.Name} - {player.Health}, броня {player.Name} - {player.Armor}");
     Console.SetCursorPosition(0, 0);
 }
+
 class User
 {
-    public int PlayerHealth = 500, PlayerArmor = 100, PotionCount = 3;
-    public string UserName;
+    public int Health = 500, Armor = 100, PotionCount = 3, heal;
+    public string Name = "Jhon Dow";
 
     public void Huganzakura(Enemy enemy)
     {
         if(enemy.assign == true)
         {
-            enemy.BossHealth -= enemy.Attack = 220 / 100 * enemy.BossArmor;
-            Console.Write($"{UserName} использовал атаку Хаганзакура и нанес {enemy.Attack} урона боссу");
+            enemy.BossHealth -= 220;
+
+            Console.Write($"{Name} использовал атаку Хаганзакура и нанес 220 урона боссу");
+
             enemy.assign = false;
         }
         else Console.Write("Босс не использовал атаку Рашамон");
     }
-    public void magicalAttack(Enemy enemy)
+    public void MagicalAttack(Enemy enemy)
     {
-        enemy.BossHealth -= enemy.Attack = 180;
-        Console.Write($"{UserName} использовал Магическую атаку и нанес {enemy.Attack} урона боссу");
-    }
-    public void Healing(Enemy enemy)
-    {
-        if (PotionCount > 0)
-        {
-            Random randomHeal = new Random();
-            PlayerHealth += enemy.heal = randomHeal.Next(25, 100); PotionCount--;
-            Console.Write($"{UserName} отхилился на {enemy.heal} хп");
-        }
-        else Console.WriteLine("Не достаточно зелий");
-    }
-}
-class Enemy
-{
+        enemy.BossHealth -= 170;
 
-    public int BossHealth = 750, BossArmor = 50, PotionCount = 3;
-    public bool assign = false; public int Attack, heal;
-    public void Rashamon(User player)
-    {
-        assign = true;
-        player.PlayerHealth -= Attack = 180 / 100 * player.PlayerArmor;
-        Console.Write($"Босс использовал атаку Рашамон и нанес {Attack} урона игроку {player.UserName}");
-    }
-    public void ShadowCloning(User player)
-    {
-        player.PlayerHealth -= Attack = 120 / 100 * player.PlayerArmor;
-        Console.Write($"Босс использовал атаку теневого клонирования и нанес {Attack} урона игроку {player.UserName}");
+        Console.Write($"{Name} использовал Магическую атаку и нанес 150 урона боссу");
     }
     public void Healing()
     {
         if (PotionCount > 0)
         {
             Random randomHeal = new Random();
-            BossHealth += heal = randomHeal.Next(25, 100); PotionCount--;
+
+            Health += heal = randomHeal.Next(25, 100); 
+            PotionCount--;
+
+            Console.Write($"{Name} отхилился на {heal} хп");
+        }
+        else Console.WriteLine("Не достаточно зелий");
+    }
+}
+
+class Enemy
+{
+    public int BossHealth = 750, BossArmor = 50, PotionCount = 3, heal;
+    public bool assign = false;
+
+    public void Rashamon(User player)
+    {
+        assign = true;
+        player.Health -= 180;
+
+        Console.Write($"Босс использовал атаку Рашамон и нанес 180 урона игроку {player.Name}");
+    }
+
+    public void ShadowCloning(User player)
+    {
+        player.Health -= 120;
+
+        Console.Write($"Босс использовал атаку теневого клонирования и нанес 120 урона игроку {player.Name}");
+    }
+
+    public void Healing()
+    {
+        if (PotionCount > 0)
+        {
+            Random randomHeal = new Random();
+
+            BossHealth += heal = randomHeal.Next(25, 100); 
+            PotionCount--;
+
             Console.Write($"Босс отхилился на {heal} хп");
         }
+
         else Console.WriteLine("Не достаточно зелий");
     }
 }
--- obj/Debug/net8.0/Boss Battle.AssemblyInfo.cs
+++ obj/Debug/net8.0/Boss Battle.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Boss Battle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58e5fa8ffd481c9689c8b8e13e6aef2144ea650a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Boss Battle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Boss Battle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
